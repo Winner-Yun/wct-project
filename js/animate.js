6d043ca25cb69d5ab4489090b2cad1174dcbf39d
Your Name <your-email@example.com>
2024-11-21 09:08:08 +0700

    add new

--- js/animate.js
+++ js/animate.js
@@ -1,24 +1,96 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // Add animation classes when the element is in view
-        entry.target.classList.add('animate__animated', 'animate__fadeInLeft');
-        observer.unobserve(entry.target);
-      }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__fadeInLeft");
+      observer.unobserve(entry.target);
+    }
   });
+});
 const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        // Add animation classes when the element is in view
-        entry.target.classList.add('animate__animated', 'animate__fadeInRight');
-        observer.unobserve(entry.target);
-      }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__fadeInRight");
+      observer.unobserve(entry.target);
+    }
   });
-  document.querySelectorAll('.sidebar').forEach(element => {
-    observer.observe(element);
+});
+document.querySelectorAll(".sidebar").forEach((element) => {
+  observer.observe(element);
+});
+document.querySelectorAll(".settings-content").forEach((element) => {
+  observer2.observe(element);
+});
+
+/*animate infokhnowledge*/
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__fadeInDown");
+      observer.unobserve(entry.target);
+    }
   });
-  document.querySelectorAll('.settings-content').forEach(element => {
-    observer2.observe(element);
+});
+document.querySelectorAll(".navbar").forEach((element) => {
+  observer3.observe(element);
+});
+
+const observer4 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__fadeInUp");
+      observer.unobserve(entry.target);
+    }
   });
+});
+document.querySelectorAll(".controlInfo").forEach((element) => {
+  observer4.observe(element);
+});
+document.querySelectorAll(".parah-tell").forEach((element) => {
+  observer.observe(element);
+});
+document.querySelectorAll(".demoVideo").forEach((element) => {
+  observer2.observe(element);
+});
+
+const observer5 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__zoomIn");
+      observer.unobserve(entry.target);
+    }
+  });
+});
+document.querySelectorAll(".box").forEach((element) => {
+  observer5.observe(element);
+});
+document.querySelectorAll(".left-panel").forEach((element) => {
+  observer.observe(element);
+});
+document.querySelectorAll(".right-panel").forEach((element) => {
+  observer2.observe(element);
+});
+const observer6 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Add animation classes when the element is in view
+      entry.target.classList.add("animate__animated", "animate__fadeInBottomLeft");
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll(".aniBack").forEach((element) => {
+  observer2.observe(element);
+});
+document.querySelectorAll(".rocket").forEach((element) => {
+  observer6.observe(element);
+});
+document.querySelectorAll(".teacher-card").forEach((element) => {
+  observer5.observe(element);
+});
