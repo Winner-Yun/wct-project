fd8498daf78451311dd360b92a4c04f5aa14358a
Your Name <your-email@example.com>
2024-11-21 17:56:47 +0700

    update new from winner

--- js/animate.js
+++ js/animate.js
@@ -94,3 +94,46 @@ document.querySelectorAll(".rocket").forEach((element) => {
 document.querySelectorAll(".teacher-card").forEach((element) => {
   observer5.observe(element);
 });
+
+
+
+/*animate loop*/
+const profileBar = document.querySelector(".profile-bar");
+const profileBtn = document.getElementById("profileBtn");
+profileBtn.addEventListener("click", function () {
+  profileBar.classList.toggle("show");
+});
+
+const scrollToTopBtn = document.getElementById('arrow-upbtn');
+
+// Add click event listener
+scrollToTopBtn.addEventListener('click', function() {
+  // Scroll to the top smoothly
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // This ensures smooth scrolling
+  });
+});
+function toggleNotificationDropdown() {
+  const dropdown = document.getElementById("notificationDropdown");
+  dropdown.style.display =
+    dropdown.style.display === "block" ? "none" : "block";
+}
+
+// Close dropdown if clicked outside
+document.addEventListener("click", function (event) {
+  const dropdown = document.getElementById("notificationDropdown");
+  const bellIcon = document.querySelector(".controlbtn a:first-child");
+
+  if (!dropdown.contains(event.target) && !bellIcon.contains(event.target)) {
+    dropdown.style.display = "none";
+  }
+});
+
+/*animation on qr code pay*/
+document.querySelectorAll(".qr-phone").forEach((element) => {
+  observer5.observe(element);
+});
+document.querySelectorAll(".payment-form").forEach((element) => {
+  observer5.observe(element);
+});
